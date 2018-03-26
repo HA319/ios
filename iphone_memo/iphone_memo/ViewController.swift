9bf048372164a7ca8981dcdaca2bf448d95f57c8
andohikaru <>
2018-03-26 22:12:48 +0900

    プロジェクト作成

---
+++ iphone_memo/iphone_memo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iphone_memo
+//
+//  Created by 安藤輝 on 2018/03/26.
+//  Copyright © 2018年 安藤輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
