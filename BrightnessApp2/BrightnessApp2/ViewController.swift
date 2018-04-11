a76310e0160e977ed784e4368d2d2f2d10aa39b5
andohikaru <>
2018-04-12 00:31:17 +0900

    プロジェクト作成

---
+++ BrightnessApp2/BrightnessApp2/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  BrightnessApp2
+//
+//  Created by 安藤輝 on 2018/04/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
