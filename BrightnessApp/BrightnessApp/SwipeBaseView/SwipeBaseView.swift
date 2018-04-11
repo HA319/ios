c77f0ff36c2a1697da20d3ca6199f8a1b0ee8074
andohikaru <>
2018-04-12 00:16:57 +0900

    完成とする

--- BrightnessApp/BrightnessApp/SwipeBaseView/SwipeBaseView.swift
+++ BrightnessApp/BrightnessApp/SwipeBaseView/SwipeBaseView.swift
@@ -8,15 +8,12 @@
 
 import UIKit
 
-// バグ1 最後までスクロールしても輝度がmaxにならない
-// バグ2 端までスクロールした後に再度スクロールすると前回のタップ位置に戻る
-// バグ3 画像がはみでてる
-
 class SwipeBaseView: UIView {
     
     @IBOutlet weak var baseView: UIView!
     @IBOutlet weak var swipeTextField: UITextField!
-    @IBOutlet weak var swipeView: UIView!
+    @IBOutlet weak var brightSwipeView: UIView!
+    @IBOutlet weak var blueLightSwipeView: UIView!
     @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
     
     var current: CGFloat = 0.0
@@ -26,12 +23,13 @@ class SwipeBaseView: UIView {
         
         initBaseView()
         initSwipeView()
+        initBlueLightSwipeView()
         initImageView()
         
         leadingConstraint.constant = baseView.frame.width * UIScreen.main.brightness
         current = leadingConstraint.constant
     }
-
+    
     private func initBaseView() {
         
         baseView.layer.cornerRadius = 15
@@ -39,28 +37,33 @@ class SwipeBaseView: UIView {
     
     private func initSwipeView() {
         
-        swipeView.layer.cornerRadius = swipeView.frame.width / 2
-        swipeView.isUserInteractionEnabled = true
-        swipeView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(swipingView)))
+        brightSwipeView.layer.cornerRadius = brightSwipeView.frame.width / 2
+        brightSwipeView.isUserInteractionEnabled = true
+        brightSwipeView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(swipingView)))
+    }
+    
+    private func initBlueLightSwipeView() {
+        
+        blueLightSwipeView.layer.cornerRadius = blueLightSwipeView.frame.width / 2
+        blueLightSwipeView.isUserInteractionEnabled = true
+        blueLightSwipeView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(swipingView)))
     }
     
     private func initImageView() {
         
-        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: swipeView.frame.width, height: swipeView.frame.height))
-        imageView.image = UIImage(named: "ExImage")?.resizeImage(width: swipeView.frame.width, height: swipeView.frame.height) // TODO: - 画像がはみ出ているから画像を直すかresizeImageでどうにかする
-        swipeView.addSubview(imageView)
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: brightSwipeView.frame.width, height: brightSwipeView.frame.height))
+        imageView.image = UIImage(named: "ExImage")?.resizeImage(width: brightSwipeView.frame.width, height: brightSwipeView.frame.height)
+        brightSwipeView.addSubview(imageView)
     }
     
     @objc func swipingView(sender: UIPanGestureRecognizer) {
 
-        let position = sender.translation(in: swipeView)
+        let position = sender.translation(in: brightSwipeView)
         // Baseを超えたらreturn
-        if (position.x + current) < 0 || (position.x + current) > baseView.frame.width - swipeView.frame.width {
+        if (position.x + current) < 0 || (position.x + current) > baseView.frame.width - brightSwipeView.frame.width {
             return
         }
         
-        // todo: 動きがおかしいなおす
-        // begainの位置に戻っている
         switch sender.state {
         case .began:
             leadingConstraint.constant = position.x + current
--- BrightnessApp/BrightnessApp/TopViewController.swift
+++ BrightnessApp/BrightnessApp/TopViewController.swift
@@ -15,19 +15,47 @@ class TopViewController: UIViewController {
     @IBOutlet weak var adView: UIView!
     @IBOutlet weak var lineTopConstraint: NSLayoutConstraint!
     
+    let contents = ["megane", "human", "newnote", "notification"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
         let swipeBaseView = UINib(nibName: "SwipeBaseView", bundle: nil).instantiate(withOwner: nil, options: nil).first as! SwipeBaseView
-//        swipeBaseView.frame.origin = CGPoint(x: 0, y: adView.frame.origin.y - swipeBaseView.frame.height - 10) // 案1 広告の上
-//        swipeBaseView.frame.origin = CGPoint(x: 0, y: 50) // 案2 画面上部(てきとうな高さ)
-//        swipeBaseView.frame.origin = CGPoint(x: 0, y: lineView.frame.origin.y - lineView.frame.height - swipeBaseView.frame.height - 10) // 案3 オレンジ線の上
         swipeBaseView.frame = CGRect(x: 0,
-                                     y: lineView.frame.origin.y - lineView.frame.height - swipeBaseView.frame.height - lineTopConstraint.constant, // 調整の10
+                                     y: lineView.frame.origin.y - lineView.frame.height - swipeBaseView.frame.height - lineTopConstraint.constant,
                                      width: view.frame.width,
                                      height: adView.frame.height * 1.5)
-
-        
         view.addSubview(swipeBaseView)
+    }
+}
+
+extension TopViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return contents.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .lightGray
+        let imageView = cell.contentView.viewWithTag(1) as! UIImageView
+        let cellImage = UIImage(named: contents[indexPath.row])
+        imageView.image = cellImage
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: collectionView.bounds.width / 2 - 0.5, height: collectionView.bounds.height / 2 - 0.5)
     }
 }
